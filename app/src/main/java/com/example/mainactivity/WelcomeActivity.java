86b5d93dd53aa9f11d7979e3347ac30b152da52b
SehamAhmed <ahme5540@mylaurier.ca>
2024-10-27 14:52:26 -0400

    10/27

---
+++ app/src/main/java/com/example/mainactivity/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.mainactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.mainactivity.databinding.ActivityWelcomeBinding;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private LinearLayout bottomPanel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
